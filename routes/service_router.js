2c56dcdc9697913d3fecc0770b3f22a9f6858ac9
hardi148 <tojohajarisoa@gmail.com>
2024-02-18 21:25:33 +0100

    web service Ajouter un service

--- routes/service_router.js
+++ routes/service_router.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const { getAllServices } = require("./objects/service");
+const { Service , getAllServices } = require("./objects/service");
 
 
 router.get('/', function(req, res, next) {
@@ -10,5 +10,18 @@ router.get('/', function(req, res, next) {
     }).catch(error => console.error('Une erreur s\'est produite', error));
 });
 
+router.post('/add', function(request,response) {
+    let service = new Service({
+        description: request.body.description,
+        prix: request.body.prix,
+        durée: request.body.durée,
+        comission: request.body.comission
+    });
+    service.save()
+        .then(() => {
+            response.json({ message: 'Service added with success' , status: '200'});
+        })
+        .catch(error => console.error('An error occurred while saving the utilisateur: ', error));
+})
 
 module.exports = router;
