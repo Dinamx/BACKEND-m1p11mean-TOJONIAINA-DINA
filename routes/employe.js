5417cf9b4b973299664b44c4bb8dc32108dc776c
hardi148 <tojohajarisoa@gmail.com>
2024-02-19 13:17:40 +0100

    terminer rendez vous

--- routes/employe.js
+++ routes/employe.js
@@ -5,6 +5,19 @@ const { Rendezvous , getHistoriqueRendezVous , getAllRendezVousEmp } = require("
 const { getAllEmploye } = require("./objects/utilisateur");
 
 
+router.put('/terminer_rendez_vous/:id', function(req, res, next) {
+    const rdvId = req.params.id;
+
+    Rendezvous.updateOne({ _id: rdvId }, { etat_rdv: 1 })
+        .then(() => {
+            res.status(200).json({ message: 'Rendez-vous terminé avec succès.' });
+        })
+        .catch(error => {
+            console.error('Une erreur s\'est produite lors de la mise à jour du rendez-vous : ', error);
+            res.status(500).json({ message: 'Une erreur s\'est produite lors de la mise à jour du rendez-vous.' });
+        });
+});
+
 router.get('/rendezvous/:emp_id', function(req, res, next) {
   const empId = req.params.emp_id;
 
