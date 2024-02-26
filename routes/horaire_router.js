a86f6f0e3984f38040fed8bb85dfd78e49eb45bf
dinarazafy <dinasly01@gmail.com>
2024-02-26 21:53:47 +0300

    Update horaire_router.js

--- routes/horaire_router.js
+++ routes/horaire_router.js
@@ -12,32 +12,43 @@ router.get('/', function(req, res, next) {
 
 // Route pour ajouter ou mettre à jour un horaire
 router.post('/add', function(request, response) {
-    const idemploye = request.body.idemploye;
+    const idemploye = request.body.idEmploye;
     const heureDebut = request.body.heureDebut;
     const heureFin = request.body.heureFin;
+
+    console.log(request.body);
 
     findByIdemploye(idemploye).then(existingHoraire => {
         if (existingHoraire) {
             // Si l'horaire existe déjà, mettez à jour
             updateHoraire(existingHoraire._id, heureDebut, heureFin)
-                .then(() => {
-                    response.json({ message: 'Horaire updated with success', status: '200' });
+                .then(updatedHoraire => {
+                    response.json({ message: 'Horaire updated with success', status: '200', horaire: updatedHoraire });
                 })
-                .catch(error => console.error('An error occurred while updating the horaire: ', error));
+                .catch(error => {
+                    console.error('An error occurred while updating the horaire: ', error);
+                    response.status(500).json({ message: 'An error occurred while updating the horaire', error: error });
+                });
         } else {
             // Si l'horaire n'existe pas, insérez-le
             insertHoraire(idemploye, heureDebut, heureFin)
-                .then(() => {
-                    response.json({ message: 'Horaire added with success', status: '200' });
+                .then(newHoraire => {
+                    response.json({ message: 'Horaire added with success', status: '200', horaire: newHoraire });
                 })
-                .catch(error => console.error('An error occurred while saving the horaire: ', error));
+                .catch(error => {
+                    console.error('An error occurred while saving the horaire: ', error);
+                    response.status(500).json({ message: 'An error occurred while saving the horaire', error: error });
+                });
         }
-    }).catch(error => console.error('An error occurred while finding the horaire: ', error));
+    }).catch(error => {
+        console.error('An error occurred while finding the horaire: ', error);
+        response.status(500).json({ message: 'An error occurred while finding the horaire', error: error });
+    });
 });
 
 router.get('/:idEmploye', function(req, res, next) {
     const idemploye = req.params.idEmploye;
-    
+
     findOrCreateHoraire(idemploye).then(horaire => {
         res.json(horaire);
     }).catch(error => {
