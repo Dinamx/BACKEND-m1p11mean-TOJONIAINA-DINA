6bd0b8d1bcdbf9eca1c80db99298bd9f501f9e1a
dinarazafy <dinasly01@gmail.com>
2024-02-26 21:15:29 +0300

    Changes For Auto Create

--- routes/objects/horaire.js
+++ routes/objects/horaire.js
@@ -14,5 +14,39 @@ function getAllHoraires()
     return Horaire.find({}).exec();
 }
 
+// Fonction pour trouver un horaire par idemploye
+function findByIdemploye(idemploye) {
+    return Horaire.findOne({ idemploye: idemploye }).exec();
+}
 
-module.exports = { Horaire,getAllHoraires};
+// Fonction pour mettre à jour un horaire existant
+function updateHoraire(id, heureDebut, heureFin) {
+    return Horaire.findByIdAndUpdate(id, { heureDebut: heureDebut, heureFin: heureFin }, { new: true }).exec();
+}
+
+// Fonction pour insérer un nouvel horaire
+function insertHoraire(idemploye, heureDebut, heureFin) {
+    let horaire = new Horaire({
+        idemploye: idemploye,
+        heureDebut: heureDebut,
+        heureFin: heureFin
+    });
+    return horaire.save();
+}
+
+function findOrCreateHoraire(idemploye) {
+    return findByIdemploye(idemploye).then(existingHoraire => {
+        if (existingHoraire) {
+            // Si l'horaire existe déjà, retourne-le
+            return existingHoraire;
+        } else {
+            // Si l'horaire n'existe pas, crée-le avec des horaires par défaut
+            return insertHoraire(idemploye, '08:00', '17:00');
+        }
+    });
+}
+
+
+
+module.exports = { Horaire, getAllHoraires, findByIdemploye, updateHoraire, insertHoraire, findOrCreateHoraire };
+
