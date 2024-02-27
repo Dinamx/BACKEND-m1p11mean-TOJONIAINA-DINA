eac9e585e95a96342e9e91315b6f6e20d841f64c
hardi148 <tojohajarisoa@gmail.com>
2024-02-27 16:52:46 +0100

    zay fa mety tsara any amin le angular le envoi mail

--- routes/offrespeciale_router.js
+++ routes/offrespeciale_router.js
@@ -10,33 +10,29 @@ router.get('/', function(req, res, next) {
     }).catch(error => console.error('Une erreur s\'est produites', error));
 });
 
-router.post('/add', function(request,response) {
+router.post('/add', function(request, response) {
+  
+  sendEmail(request.body.mail_envoi, request.body.date_heure_envoi, request.body.contenu, request.body.pourcentage);
+      
+  let offrespeciale = new Offrespeciale({
+      idclient: request.body.idclient,
+      contenu: request.body.contenu,
+      date_heure_envoi: request.body.date_heure_envoi,
+      mail_envoi: request.body.mail_envoi,
+      pourcentage: request.body.pourcentage,
+      date_fin: request.body.date_fin,
+      idservice: request.body.idservice
+  });
 
-    let offrespeciale = new Offrespeciale({
-        idclient: request.body.idclient,
-        contenu: request.body.contenu,
-        date_heure_envoi: request.body.date_heure_envoi,
-        mail_envoi: request.body.mail_envoi,
-        pourcentage: request.body.pourcentage,
-        date_fin: request.body.date_fin,
-        idservice: request.body.idservice
-    });
-    offrespeciale.save()
-        .then(() => {
-            response.json({ message: 'Offrespeciale added with success' , status: '200'});
-            
-            var now = new Date();
-            var targetDate = offrespeciale.date_heure_envoi;
-            var diffInMilliseconds = targetDate - now;
-          
-            setTimeout(function() {
-              sendEmail(offrespeciale.mail_envoi,targetDate,offrespeciale.contenu,offrespeciale.pourcentage);
-            }, diffInMilliseconds);
-           
-            response.status(201).send('Email will be sent at the specified date and time.');
+  offrespeciale.save()
+      .then(() => {
+          response.status(201).json({ message: 'Offrespeciale added with success', status: '200' });
+      })
+      .catch(error => {
+          console.error('An error occurred while saving an offrespeciale: ', error);
+          response.status(500).json({ error: 'An error occurred while saving an offrespeciale' });
+      });
+});
 
-        })
-        .catch(error => console.error('An error occurred while saving an offrespeciale: ', error));
-})
 
 module.exports = router;
