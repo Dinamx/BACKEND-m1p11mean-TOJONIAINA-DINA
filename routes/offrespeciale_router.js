ffc0b561f2a5367032c107dcd43b52c6220099b3
hardi148 <tojohajarisoa@gmail.com>
2024-02-29 14:04:45 +0100

    manova an le maka date ao amin offre speciale

--- routes/offrespeciale_router.js
+++ routes/offrespeciale_router.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment');
 
 const { Offrespeciale , getAllOffres , getPourcentageOffre } = require("./objects/offrespeciale");
 const { sendEmail } = require("./helpers/mailSender");
@@ -13,14 +14,23 @@ router.get('/', function(req, res, next) {
 router.post('/add', function(request, response) {
   
   sendEmail(request.body.mail_envoi, request.body.date_heure_envoi, request.body.contenu, request.body.pourcentage);
-      
+  
+
+  const momentDateHeure = moment.utc(request.body.date_heure_envoi);
+  const date_heure = momentDateHeure.add(1, 'hour');
+  date_heure.subtract(1, 'hour');
+
+  const momentDateHeureFin = moment.utc(request.body.date_fin);
+  const date_fin = momentDateHeureFin.add(1, 'hour');
+  date_fin.subtract(1, 'hour');
+
   let offrespeciale = new Offrespeciale({
       idclient: request.body.idclient,
       contenu: request.body.contenu,
-      date_heure_envoi: request.body.date_heure_envoi,
+      date_heure_envoi: date_heure,
       mail_envoi: request.body.mail_envoi,
       pourcentage: request.body.pourcentage,
-      date_fin: request.body.date_fin,
+      date_fin: date_fin,
       idservice: request.body.idservice
   });
 
